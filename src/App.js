9afc52dc5618eebb9e121b8df2d9ce0546c2af5d
developersrinu <orsusrinu606@gmail.com>
2023-08-11 07:52:41 +0530

    d

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Iam ready 
+          Iam ready lunch
         </a>
       </header>
     </div>
